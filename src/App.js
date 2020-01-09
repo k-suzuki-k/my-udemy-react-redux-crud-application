53fd4965796598d54dc43ff34c8e77a3272a1333
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2020-01-09 21:38:42 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "Hanao", age: 15 },
-    { name: "NoName", age: 55 }
-  ]
-  return (
-    <div>
-    {
-      profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index}/>
-      })
-    }
-    </div>
-  )
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+  hundlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  hundleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <div>Count: { this.state.count }</div>
+        <button onClick={this.hundlePlusButton}>+1</button>
+        <button onClick={this.hundleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
