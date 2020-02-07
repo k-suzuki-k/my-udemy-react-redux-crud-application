11e08acfcd4f24fe47e0aed4bd6be7a82abf415b
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2020-02-07 16:07:10 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions';
 
-const App = () => (<Counter></Counter>)
-
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { count: 0 }
-  }
-
-  hundlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-  hundleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-
+class App extends Component {
   render () {
+    const props = this.props;
+
     return (
       <React.Fragment>
-        <div>Count: { this.state.count }</div>
-        <button onClick={this.hundlePlusButton}>+1</button>
-        <button onClick={this.hundleMinusButton}>-1</button>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value });
+const mapDispathToProps = dispath => ({
+  increment: () => dispath(increment()),
+  decrement: () => dispath(decrement())
+})
+
+export default connect(mapStateToProps, mapDispathToProps)(App);
+
