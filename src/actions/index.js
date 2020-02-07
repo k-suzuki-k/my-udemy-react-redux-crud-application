0d91673f04c5a6f6f91ee914d01820a585cd7d03
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2020-02-07 14:47:51 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export cont increment = () => ({
+  type: INCREMENT
+})
+
+export cont decrement = () => ({
+  type: DECREMENT
+})
